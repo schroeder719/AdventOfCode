cd14ea1253803de013283fc883b36d90e7f44f5c
Andrew Schroeder <andrewrschroeder@gmail.com>
2020-12-09 00:44:45 -0500

    day 9

--- 2020/9/9a.py
+++ 2020/9/9a.py
@@ -2,9 +2,10 @@ import os
 import re
 import common.util as u
 import common.comp as c
+import logging
 
 
-data = u.readfile(u.AOC_2020 + "\\8\\input.txt")
+data = u.readfile(u.AOC_2020 + "\\9\\input.txt")
 tl = 0
 for lines in data:
     tl+=1
@@ -12,25 +13,56 @@ chg = 0
 
 comp = c.Computer()
 
-for i in range(0,tl):
-    print("i:{}".format(i))
-    p = data.copy()
-    if "jmp" in p[i]:
-        cmd = p[i].split(" ")
-        p[i] = 'nop' + " " + cmd[1]
-        print(p[i])
-        res,acc = comp.load_run(p)
-                    
-    elif "nop" in p[i]:
-        cmd = p[i].split(" ")
-        p[i] = 'jmp' + " " + cmd[1]
-        print(p[i])
-        res,acc = comp.load_run(p)
-    else:
-        continue
+def test(i):
+    for j in range(i-25,i):
+        for k in range(i-24,i):
+            if (i != j):
+                print("{} {}, {}+{}={}".format(j,k,data[j],data[k],data[j]+data[k]))
+                if int(data[j])+int(data[k]) == int(data[i]):
+                    return True
+    return False
+def part1():
+    for i in range(25,len(data)):
+        print(data[i])
+        if test(i) == False:
+            print("no match: {}".format(data[i]))
+            exit()
 
-    if res == True:
-        print("finished")
-        print("acc: {}".format(acc))
-        exit(0)
+def part2():
+    found = False
+    target = 1639024365
+    i = 651
+    while not found:
+        i-=1
+        print(i)
+        total = int(data[i])
+        j = i
+
+        while (total < target and j >0):
+            j-=1
+            total += int(data[j])
+            
+        
+        if total == target:
+            print("total: {} target: {}".format(total,target))
+            found = True
+            print("{},{}".format(i,j))
+            t = 0
+            mn = target
+            mx = 0
+            for l in range(j,i+1):
+                a = int(data[l])
+                t += a
+                if a > mx:
+                    mx = a
+                if a < mn:
+                    mn = a
+            print(t)
+            print("{}+{}={}".format(mn,mx, mx+mn))
+
+part2()
+
+
+
+
     
